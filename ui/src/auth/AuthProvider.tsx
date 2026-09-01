d2075c53e1f142dcfdea27d341cbe72ccecdd30c
Olivia Knestaut <olivia.knestaut@gmail.com>
2026-08-31 23:22:53 -0400

    fixing more changes :(

--- ui/src/auth/AuthProvider.tsx
+++ ui/src/auth/AuthProvider.tsx
@@ -13,9 +13,7 @@ import { AuthContext, type User } from './AuthContext';
 function withTimeout<T>(promise: Promise<T>, ms = 8000): Promise<T> {
   return Promise.race([
     promise,
-    new Promise<T>((_, reject) =>
-      setTimeout(() => reject(new Error('Request timed out')), ms)
-    ),
+    new Promise<T>((_, reject) => setTimeout(() => reject(new Error('Request timed out')), ms)),
   ]);
 }
 
